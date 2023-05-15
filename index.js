c805bda7b691422feedb01403bb648682aeb2c1c
ckgathari <Chege>
2023-05-15 23:52:08 +0300

    changed validation in js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"
 ];
  
 function getName(){
-    var year=parseInt(document.getElementById("year").value);
+    var year =parseInt(document.getElementById("year").value);
     var month =parseInt(document.getElementById("month").value);
-    var day=parseInt(document.getElementById("day").value);
+    var day =parseInt(document.getElementById("day").value);      
     var male = document.getElementById("male");
     var female = document.getElementById("female");
     //validation
